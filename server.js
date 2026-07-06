2795116d6a5be03a64e9cc8495763b8fe69702a4
secilyun <secilyun@gmail.com>
2026-07-06 20:55:14 +0900

    fix: CORS 헤더 추가 (Toss WebView → Railway 호출 허용)

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 app.use(express.json());
 app.use(express.static('public'));
 
